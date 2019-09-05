931457b513c411e4df6b9dd4e921ef81719f37fa
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-09-04 23:09:16 -0300

    new: config database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: '172.17.0.2',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'calcamaisNodeJs',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
