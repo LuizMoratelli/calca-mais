6992dd66ef57c2487497544a0b199d7a3cae22e4
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-23 19:39:48 -0300

    new: init project structure

--- client/src/styles/global.js
+++ client/src/styles/global.js
@@ -13,19 +13,15 @@ export default createGlobalStyle`
         background: #191920 url(${background}) no-repeat center top;
         -webkit-font-smoothing: antialiased;
     }
-
     body, input, button{
         font: 14px Roboto, sans-serif;
     }
-
     #root{
         max-width: 1020px;
         margin: 0 auto;
         padding: 0 20px 50px;
     }
-
     button{
         cursor: pointer;    
     }
-
 `;
--- client/src/util/format.js
+++
@@ -1,4 +0,0 @@
-export const { format: formatPrice } = new Intl.NumberFormat('pt-Br', {
-    style: 'currency',
-    currency: 'BRL',
-});
