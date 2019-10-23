6198d6e8eda0d3d6da0da76369e84c4d667cdf4c
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-23 20:20:49 -0300

    new: config axios to use api request

---
+++ client/src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000',
+})
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('@calcamais:token');
+    const headers = {...config.headers };
+
+    if(token){
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    return { ...config, headers};
+})
+
+export default api;
