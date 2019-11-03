188d61361aae2c01732d5d50d68387fc094f19ba
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-03 10:48:59 -0300

    chore: config App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import ReduxToastr from 'react-redux-toastr';
+import { Provider } from 'react-redux';
+import Routes from './routes';
+import store from './store';
+import GlobalStyle from './styles/global';
 
 const App = () => (
-  <div className="App">
-  </div>
+  <Provider store={store}>
+    <>
+      <Routes />
+      <ReduxToastr />
+      <GlobalStyle/>
+    </>
+
+  </Provider>
 )
 
 export default App
