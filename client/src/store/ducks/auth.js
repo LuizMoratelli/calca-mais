7da6e7dc95894e99a1d154aedf2104a53626a955
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-03 10:38:27 -0300

    new: ducks auth

---
+++ client/src/store/ducks/auth.js
@@ -0,0 +1,33 @@
+import { createReducer, createActions } from 'reduxsauce'
+import Immutable from 'seamless-immutable';
+
+const { Types, Creators } = createActions({
+    signInRequest: ['email', 'password'],
+    signInSuccess: ['token'],
+    signOut: null,
+    signUpRequest: ['nome', 'email', 'password'],
+});
+
+
+export const AuthTypes = Types;
+export default Creators;
+
+const INITIAL_STATE = Immutable({
+    signedIn: !!localStorage.getItem('@CalcaMais:token'),
+    token: localStorage.getItem('@CalcaMais:token') || null,
+    roles: [],
+    permissions: [],
+});
+
+export const success = (state, {token}) => {
+    console.log(token);
+    return state.merge({ signedIn: true, token })
+}
+
+export const logout = (state) => state.merge({ signedIn: false, token: null });
+
+
+export const reducer = createReducer(INITIAL_STATE, {
+    [Types.SIGN_IN_SUCCESS]: success,
+    [Types.SIGN_OUT]: logout,
+});
