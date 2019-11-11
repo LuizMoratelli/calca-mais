11e2e64d2b0186636a0cee6520e8cfda51a97adc
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-11 00:02:30 -0300

    new: add page categorias

---
+++ client/src/pages/Categorias/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from "../../components/Header";
+import { Container } from './styles'
+
+const Category = () => ( 
+  <>
+    <Header/>
+    <Container>
+        
+    </Container>
+  </>
+)
+
+export default Category
