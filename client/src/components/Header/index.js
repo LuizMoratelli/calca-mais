c0fa6f96df6992e06539df39f315a43c83f6770f
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-10 23:58:33 -0300

    config: Header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { MdShoppingBasket } from 'react-icons/md'
-import { Container, Cart } from './styles';
+import { MdShoppingBasket, MdAccountCircle, MdStorage, MdBorderColor } from 'react-icons/md'
+import { Container, Opcao, Logo} from './styles';
 
-import logo from '../../assets/images/logo.png';
+import logo from '../../assets/images/logo2.png';
 
-function Header({ cartSize }) {
+function Header() {
   return (
     <Container>
       <Link to="/">
-        <img src={logo} alt="calça mais" />
+        <Logo src={logo} alt="calça mais" />
       </Link>
-      <Cart to="/cart">
+      <Opcao to="/pedidos">
         <div>
-          <strong>Meu carrinho</strong>
-          <span>{cartSize} itens</span>
+          <strong>Pedidos</strong>
+          <span>itens</span>
         </div>
         <MdShoppingBasket size={36} color="#fff"/>
-      </Cart>
+      </Opcao>
+      <Opcao to="/usuarios">
+        <div>
+          <strong>Usuarios</strong>
+          <span>itens</span>
+        </div>
+        <MdAccountCircle size={36} color="#fff"/>
+      </Opcao>
+      <Opcao to="/categorias">
+        <div>
+          <strong>Categorias</strong>
+          <span>itens</span>
+        </div>
+        <MdStorage size={36} color="#fff"/>
+      </Opcao>
+      <Opcao to="/calcados">
+        <div>
+          <strong>Calçados</strong>
+          <span>itens</span>
+        </div>
+        <MdBorderColor size={36} color="#fff"/>
+      </Opcao>
     </Container>
   );
 }
 
-export default connect(state => ({
-  cartSize: state.cart.length
-}))(Header)
+export default Header;
