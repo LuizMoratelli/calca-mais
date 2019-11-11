c5f66fa286adf2e2af3f987d9e00750bba3a0791
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-11 00:00:05 -0300

    chore: config main page

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { MdAddShoppingCart } from "react-icons/md";
+import { ProductList } from "./styles";
+import { formatPrice } from "../../util/format";
+import api from "../../services/api";
 
-import { Container } from './styles'
+import Header from "../../components/Header";
 
-const Main = () => ( 
-  <Container>
-      
-  </Container>
-)
+export default function Main() {
+  const [produtos, setProdutos] = useState();
 
-export default Main
+  useEffect(() => {
+    async function getProdutos() {
+      const response = await api.get("products");
+      // formata o preço uma única vez e matém o
+      // restante dos produtos, apenas adicona uma nova propriedade
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormated: formatPrice(product.price)
+      }));
+      setProdutos(data);
+    }
+  }, []);
+
+  function handleAddProduto(id) {
+    const { addToCartRequest } = this.props;
+
+    addToCartRequest(id);
+  }
+  return (
+    <>
+      <Header />
+      <ProductList>
+        {produtos && (
+          produtos.map(produto => (
+            <li key={produto.id}>
+              <img src={produto.image} alt={produto.title} />
+              <strong>{produto.title}</strong>
+              <span>{produto.priceFormated}</span>
+              <button
+                type="button"
+                onClick={() => handleAddProduto(produto.id)}
+              >
+                <div>
+                  <MdAddShoppingCart size={16} color="#FFF" />{" "}
+                  {produto.id || 0}
+                </div>
+  
+                <span>Adiconar ao carrinho</span>
+              </button>
+            </li>
+          ))
+        )}
+      </ProductList>
+    </>
+  );
+}
