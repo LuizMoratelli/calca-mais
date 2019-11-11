3e5e684b0262ba6850cfc4b824a2fff8ce54d4c2
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-11 19:42:22 -0300

    chore: config position Header component

--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -5,11 +5,11 @@ export const Container = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 50px 0;
+  margin: 20px 0;
 `;
 
 export const Logo = styled.img`
-  height: 100px;
+  /* height: 100px; */
 `;
 
 export const Opcao = styled(Link)`
