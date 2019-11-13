39245b0eb075076a82ab7b575ac0fad3cc37b157
luizmoratelli <gameplayteam12@gmail.com>
2019-11-13 14:16:00 -0300

    chore(pedido): fix usuario

--- server/app/Http/Controllers/PedidoController.php
+++ server/app/Http/Controllers/PedidoController.php
@@ -7,27 +7,37 @@ use App\{Pedido, Usuario};
 
 class PedidoController extends Controller
 {
-  public function index(Usuario $usuario) {
+  public function index(Request $request) {
+    $usuario = Usuario::find($request->usuario_id);
+
     return response()->json($usuario->pedidos);
   }
 
-  public function store(Request $request, Usuario $usuario) {
+  public function store(Request $request) {
+    $usuario = Usuario::find($request->get('usuario_id'));
+
     $pedido = $usuario->pedidos()->create($request->all());
 
     return response()->json($pedido->toArray(), 200);
   }
 
-  public function show(Usuario $usuario ,Pedido $pedido) {
+  public function show(Pedido $pedido) {
+    $usuario = Usuario::find($request->get('usuario_id'));
+
     return $usuario->pedidos->find($pedido);
   }
 
-  public function update(Request $request, Pedido $pedido, Usuario $usuario) {
+  public function update(Request $request, Pedido $pedido) {
+    $usuario = Usuario::find($request->get('usuario_id'));
+
     $pedido = $usuario->pedidos->find($pedido->id)->update($request->all());
 
     return response()->json($pedido, 200);
   }
 
-  public function destroy(Pedido $pedido, Usuario $usuario) {
+  public function destroy(Pedido $pedido) {
+    $usuario = Usuario::find($request->get('usuario_id'));
+
     $usuario->pedidos->find($pedido)->delete();
 
     return response('', 204);
